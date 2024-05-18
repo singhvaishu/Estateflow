1b7921d5569b09d9a0bd0c5ad7ff0549adf1224f
singhvaishu <vaishali231305@gmail.com>
2024-05-18 09:53:05 +0530

    url change local to producation

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-q0h0gw3ao1j353ab.us.auth0.com"
-     clientId="yZz0NMdZ7IJ35figaAcjNQfHPbBoMj1y"
-     authorizationParams={{
-      redirect_uri: "http://localhost:5173/"
-     }}
-     audience="http://localhost:8000"
-     scope="openid profile email"
+      domain="dev-q0h0gw3ao1j353ab.us.auth0.com"
+      clientId="yZz0NMdZ7IJ35figaAcjNQfHPbBoMj1y"
+      authorizationParams={{
+        redirect_uri: "https://estateflow.vercel.app/"
+      }}
+      audience="https://estateflow-backend.vercel.app"
+      scope="openid profile email"
     >
       <App />
     </Auth0Provider>
